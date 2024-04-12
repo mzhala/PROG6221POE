e41c282d19e2d7d1a0834510c60decaf25a14c17
HalaMzobe <st10355256@rcconnect.edu.za>
2024-04-12 05:56:29 +0200

    Message 2:
    Add menu to Main Program
    
    Added a menu system to the main program to provide user options
    -Added options: Add Recipe, View Recipe, Scale Recipe, Remove Recipe and Exit.
    - Prompt user to enter their choice

--- Program.cs
+++ Program.cs
@@ -34,10 +34,56 @@ class Recipe
     }
 }
 
+// Main program
 class Program
 {
     static void Main(String[] args)
     {
-        Console.WriteLine("Dine.Me Menu");
+        // Main loop of the program
+        string choice = "";
+        while(choice != "5")
+        {
+            // Display menu options
+            Console.WriteLine("\nDine.Me Menu");
+            Console.WriteLine("1. Add Recipe");
+            Console.WriteLine("2. View Recipe");
+            Console.WriteLine("3. Scale Recipe");
+            Console.WriteLine("4. Remove Recipe");
+            Console.WriteLine("5. Exit");
+
+            //Read user choice from input
+            Console.Write("Enter your choice: ");
+            choice = Console.ReadLine();
+
+            switch(choice)
+            {
+                case "1":
+                    // Call Add Recipe method when user chooses option 1
+
+                    break;
+                case "2":
+                    // Call ViewRecipe method when user chooses option 2
+
+                    break;
+                case "3":
+                    // Call ScaleRecipe method when user chooses option 3
+                    
+                    break;
+                case "4":
+                    // Call RemoveRecipe method when user chooses option 4
+
+                    break;
+                case "5":
+                    // Exit the program when the user chooses option 5
+
+                    Environment.Exit(0);
+                    break;
+                default:
+                    // Display message for invalid input
+
+                    Console.WriteLine("Invalid choice. please try again");
+                    break;
+            }
+        }        
     }
 }
