038324cda2b24053755a617c77377536f1885db0
HalaMzobe <HalaMzobe@HALAMZ>
2024-04-12 04:16:19 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Linq.Expressions;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Dine.Me Menu");
+    }
+}
