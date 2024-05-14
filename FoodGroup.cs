2f90c7b69d7dceb389e9304b9a171a6a44751b06
HalaMzobe <st10355256@rcconnect.edu.za>
2024-05-14 20:02:05 +0200

    Added -  FoodGroup class
    The FoodGroup class represents a single food group with its name, description, and examples

---
+++ FoodGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Base class representing an FoodGroup in a FoodGroups
+namespace PROG6221POE
+{
+    public class FoodGroup
+    {
+        public string Name { get; }
+        public string Description { get; }
+        public List<string> Examples { get; }
+
+        public FoodGroup(string name, string description, List<string> examples)
+        {
+            Name = name;
+            Description = description;
+            Examples = examples;
+        }
+    }
+}
